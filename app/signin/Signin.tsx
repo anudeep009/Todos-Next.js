bfe084d5cf5208eedbe573ce2c36196cd3663e8d
[anudeep avula] <[anudeepavula009@gmail.com]>
2024-12-27 17:41:33 +0530

    project initialization

---
+++ app/signin/Signin.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+function Signin() {
+  return (
+    <div>
+        <Card>
+          <CardHeader>
+            <CardTitle>Account</CardTitle>
+            <CardDescription>
+              Log in to your account
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="space-y-2">
+            <div className="space-y-1">
+              <Label htmlFor="name">Username</Label>
+              <Input id="name" />
+            </div>
+            <div className="space-y-1">
+              <Label htmlFor="username">Password</Label>
+              <Input id="username" />
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button>Signin</Button>
+          </CardFooter>
+        </Card>
+    </div>
+  )
+}
+
+export default Signin
