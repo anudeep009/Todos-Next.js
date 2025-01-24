67bb8fcef298cb22289706d293459e54d39333ab
[anudeep avula] <[anudeepavula009@gmail.com]>
2025-01-24 06:59:28 +0530

    added ui components

--- prisma/prisma.ts
+++ prisma/prisma.ts
@@ -6,4 +6,16 @@ const prisma = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
+async function verifyDbConnection() {
+  try {
+    await prisma.$connect();
+    console.log('Database connected successfully!');
+  } catch (error) {
+    console.error('Failed to connect to the database:', error);
+    process.exit(1);
+  }
+}
+
+verifyDbConnection();
+
 export default prisma;
---
+++ src/app/login/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import LoginForm from "./LoginForm"
+
+const Page = () => {
+  return (
+    <>
+      <LoginForm />
+    </>
+  )
+}
+
+export default Page
